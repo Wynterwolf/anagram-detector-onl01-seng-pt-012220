38a5c41421281bd0a1780a8d5f2e9eb588f9f475
Samantha Jordan <shinyguns@gmail.com>
2020-02-11 04:28:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,12 @@ class Anagram
     @words = word
   end
   
-  
+  def match(word_array)
+    #in the word array for each word do
+    word_array.select do |word|
+      # a split and a sort, check if matches
+      word.split("").sort == @word.split("").sort
+    end
+  end
   
 end
