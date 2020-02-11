4dbbd115efddecbeb2a71ba751d7353b3641dd1e
Samantha Jordan <shinyguns@gmail.com>
2020-02-11 04:32:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,21 +1,17 @@
-# Your code goes here!
-
 class Anagram
+  attr_accessor :word_to_analyze
   
-  attr_accessor :words
+  def initialize(word_to_analyze)
+    @word_to_analyze = word_to_analyze 
+  end 
   
-  def initialize(word)
-    @words = word
-  end
-  
-  def match(word_array)
-  
-    word_array.find_all do |word|
-      
-      if word.split("").sort == @word.split("").sort
-        word
+  def match(find_anagram_in_this_string) 
+    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    find_anagram_in_this_string.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
+        word 
       end
-    end
+    end 
   end
   
 end
